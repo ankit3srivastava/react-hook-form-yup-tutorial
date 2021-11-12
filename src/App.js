3cf61020921b1414da0d0c5c250077c2e17e5b91
Ankit Srivastava <ankit3srivastava@gmail.com>
2021-11-12 17:37:37 +0530

    date validation added

--- src/App.js
+++ src/App.js
@@ -6,13 +6,17 @@ function App() {
   let schema = yup.object().shape({
     firstname: yup.string().required(),
     email: yup.string().required().email(),
-    password: yup.string().required().min(6)
-  })
+    password: yup.string().required().min(6),
+    openingDate: yup.date().required("Please enter the required field"),
+    closingDate: yup.date()
+      .min(yup.ref("openingDate"))
+      .required("Please enter the required field"),
+  });
   const { register, handleSubmit, errors } = useForm({
     mode: "onTouched",
-    validationSchema: schema
+    validationSchema: schema,
   });
-  const onSubmit = (data) => console.log("submited :",data);
+  const onSubmit = (data) => console.log("submited :", data);
   return (
     <div className="App">
       <form onSubmit={handleSubmit(onSubmit)}>
@@ -24,7 +28,9 @@ function App() {
           // })}
           ref={register}
         />
-        {errors.firstname?.type === "required" && <p style={{color: "red"}}>{errors.firstname.message}</p>}
+        {errors.firstname?.type === "required" && (
+          <p style={{ color: "red" }}>{errors.firstname.message}</p>
+        )}
         <br />
         <input
           type="text"
@@ -35,8 +41,12 @@ function App() {
           // })}
           ref={register}
         />
-        {errors.email?.type === "required" && <p style={{color: "red"}}>{errors.email.message}</p>}
-        {errors.email?.type === "email" && <p style={{color: "red"}}>{errors.email.message}</p>}
+        {errors.email?.type === "required" && (
+          <p style={{ color: "red" }}>{errors.email.message}</p>
+        )}
+        {errors.email?.type === "email" && (
+          <p style={{ color: "red" }}>{errors.email.message}</p>
+        )}
         <br />
         <input
           type="password"
@@ -47,8 +57,22 @@ function App() {
           // })}
           ref={register}
         />
-        {errors.password?.type === "required" && <p style={{color: "red"}}>{errors.password.message}</p>}
-        {errors.password?.type === "min" && <p style={{color: "red"}}>{errors.password.message}</p>}
+        {errors.password?.type === "required" && (
+          <p style={{ color: "red" }}>{errors.password.message}</p>
+        )}
+        {errors.password?.type === "min" && (
+          <p style={{ color: "red" }}>{errors.password.message}</p>
+        )}
+        <br />
+        <input type="date" name="openingDate" ref={register}/>
+        {errors.openingDate?.type === "required" && (
+          <p style={{ color: "red" }}>{errors.openingDate.message}</p>
+        )}
+        <br />
+        <input type="date" name="closingDate" ref={register}/>
+        {errors.closingDate?.type === "required" && (
+          <p style={{ color: "red" }}>{errors.openingDate.message}</p>
+        )}
         <br />
         <button type="submit">Submit</button>
       </form>
